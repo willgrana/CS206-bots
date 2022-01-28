05a61a11c8343477355c93b16dfcd4f37bc31e45
willgrana <wgrana@uvm.edu>
2022-01-28 09:57:26 -0500

    tested gravity

--- simulate.py
+++ simulate.py
@@ -5,9 +5,13 @@ import time
 
 physicsClient = p.connect(p.GUI)
 
+p.setGravity(0,0,-9.8)
+p.loadSDF("box.sdf")
+
 for i in range(1000):
 	p.stepSimulation()
 	time.sleep(1/40)
 	print(i)
+
 p.disconnect()
 
