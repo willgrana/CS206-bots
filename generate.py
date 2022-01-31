1a60cab6b5dd9d02b4dc0360c17ab07b0b3bf212
willgrana <wgrana@uvm.edu>
2022-01-31 12:08:17 -0500

    towers

--- generate.py
+++ generate.py
@@ -4,20 +4,22 @@ import pyrosim.pyrosim as pyrosim
 
 pyrosim.Start_SDF("boxes.sdf")
 
-length = 1
-width = 1
-height = 1
-
 x = 0
 y = 0
 z = 0.5
 
-pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length,width,height])
-
-x2 = 1
-y2 = 0
-z2 = 1.5
-
-pyrosim.Send_Cube(name="Box2", pos=[x2,y2,z2], size = [length,width,height])
-
+for i in range(5):
+	x=i
+	for j in range(5):
+		y=j
+		l = 1
+		w = 1
+		h = 1
+		for k in range(10):
+			z=k+0.5
+			pyrosim.Send_Cube(name="Box",pos=[x,y,z],size=[l,w,h])
+			l = l*0.9
+			w = w*0.9
+			h = h*0.9
+			print("z:" +str(z))
 pyrosim.End()
